3c7c5f54d61c0aa236820f096b08fee97d95c9ba
yogendra0914 <yogensubedi1855@gmail.com>
2024-04-29 17:17:33 +0545

    fixed overflow issue

--- lib/screens/employee.dart
+++ lib/screens/employee.dart
@@ -45,70 +45,73 @@ class _EmployeeState extends State<Employee> {
           ],
         ),
       ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              FormFieldName(title: "Name"),
-              SizedBox(height: 10.0),
-              FieldContainer(controller: nameController),
-              SizedBox(height: 20.0),
-              FormFieldName(title: "Age"),
-              SizedBox(height: 10.0),
-              FieldContainer(controller: ageController),
-              SizedBox(height: 20.0),
-              FormFieldName(title: "Location"),
-              SizedBox(height: 10.0),
-              FieldContainer(controller: locationController),
-              SizedBox(height: 20.0),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () async {
-                    String Id = randomAlpha(10);
-                    Map<String, dynamic> employeeInfoMap = {
-                      "Name": nameController.text,
-                      "Age": ageController.text,
-                      "Id": Id,
-                      "Location": locationController.text,
-                    };
-                    try {
-                      await DatabaseMethods()
-                          .addEmployeeDetail(employeeInfoMap, Id);
-                      Fluttertoast.showToast(
-                        msg: "Employee detail has been uploaded successfully",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.CENTER,
-                        timeInSecForIosWeb: 1,
-                        backgroundColor: Colors.green,
-                        textColor: Colors.white,
-                        fontSize: 16.0,
-                      );
-                    } catch (e) {
-                      print("Error uploading employee detail: $e");
-                      Fluttertoast.showToast(
-                        msg: "Error uploading employee detail",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.CENTER,
-                        timeInSecForIosWeb: 1,
-                        backgroundColor: Colors.red,
-                        textColor: Colors.white,
-                        fontSize: 16.0,
-                      );
-                    }
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    "Add",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FormFieldName(title: "Name"),
+                SizedBox(height: 10.0),
+                FieldContainer(controller: nameController),
+                SizedBox(height: 20.0),
+                FormFieldName(title: "Age"),
+                SizedBox(height: 10.0),
+                FieldContainer(controller: ageController),
+                SizedBox(height: 20.0),
+                FormFieldName(title: "Location"),
+                SizedBox(height: 10.0),
+                FieldContainer(controller: locationController),
+                SizedBox(height: 20.0),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      String Id = randomAlpha(10);
+                      Map<String, dynamic> employeeInfoMap = {
+                        "Name": nameController.text,
+                        "Age": ageController.text,
+                        "Id": Id,
+                        "Location": locationController.text,
+                      };
+                      try {
+                        await DatabaseMethods()
+                            .addEmployeeDetail(employeeInfoMap, Id);
+                        Fluttertoast.showToast(
+                          msg: "Employee detail has been uploaded successfully",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.green,
+                          textColor: Colors.white,
+                          fontSize: 16.0,
+                        );
+                      } catch (e) {
+                        print("Error uploading employee detail: $e");
+                        Fluttertoast.showToast(
+                          msg: "Error uploading employee detail",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.red,
+                          textColor: Colors.white,
+                          fontSize: 16.0,
+                        );
+                      }
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Add",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
